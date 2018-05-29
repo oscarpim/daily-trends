d60e84d94c88e1f1e53a5d25d4bbea7f5371b4fe
oscarpim <oscarpimillet@gmail.com>
2018-05-29 13:44:31 +0200

    Implementados los servicios CRUD del Controlador

--- app/Http/Controllers/CrudController.php
+++ app/Http/Controllers/CrudController.php
@@ -3,8 +3,57 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Feed;
 
 class CrudController extends Controller
 {
-    //Controlador CRUD del modelo Feed
+    //Controladores CRUD del modelo Feed
+    
+    public function create(Request $request){
+        
+        $feed = new Feed();
+        
+        $feed -> title = $request -> title;
+        $feed -> body = $request -> body;
+        $feed -> image = $request -> image;
+        $feed -> source = $request -> source;
+        $feed -> publisher = $request -> publisher;
+        
+        $feed -> save();
+        
+        return redirect('/');
+    }
+    
+    public function read(){
+        
+        $feeds = Feed::all();
+        
+        return view('feed.index', ['feeds' => $feeds]);
+    }
+    
+    public function update(Requet $request, $id){
+        
+        $feed = Feed::findOrFail($id);
+        
+        $feed -> title = $request -> title;
+        $feed -> body = $request -> body;
+        $feed -> image = $request -> image;
+        $feed -> source = $request -> source;
+        $feed -> publisher = $request -> publisher;
+        
+        $feed -> save();
+        
+        return redirect('/');
+    }
+    
+    public function delete($id){
+        
+        $feed = Feed::findOrFail($id);
+        $feed -> delete();
+        
+        return redirect('/');
+        
+    }
 }
+
+?>
