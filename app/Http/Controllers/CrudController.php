4f65ed93b2ae389c3f96e5b6ed9b3883c5100dc5
oscarpim <oscarpimillet@gmail.com>
2018-05-30 12:35:52 +0200

    Scrapping en controlador 5 noticias el mundo utilizando Goutte

--- app/Http/Controllers/CrudController.php
+++ app/Http/Controllers/CrudController.php
@@ -4,9 +4,33 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Feed;
+use Goutte\Client;
 
 class CrudController extends Controller
 {
+    
+    public function scraper(){
+        
+        $client = new Client();
+        // Vamos al periodico el Pais
+        $elMundo = $client->request('GET', 'http://www.elmundo.es/');
+        
+        // Cogemos los ultimos posts de la portada y mostramos los titulos
+        $titulosMundo = array();
+        $elMundo->filter('h2 > a')->each(function ($node) use (&$titulosMundo) {
+             $titulosMundo[] = $node->text()."<br><br>";
+        });
+        //Se almacenan en un array para luego mostrar el limite de 5 noticias que queremos
+        $i=0;
+        do{
+           print $titulosMundo[$i]; 
+           $i++;
+        }while($i<5);
+        
+        
+    }
+    
+    
     //Controladores CRUD del modelo Feed
     
     public function create(Request $request){
